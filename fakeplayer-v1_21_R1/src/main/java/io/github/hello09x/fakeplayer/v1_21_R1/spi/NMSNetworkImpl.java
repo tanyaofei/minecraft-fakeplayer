877404b3e0d743ffbd358a9cfae38deaa3e949c3
tanyaofei <tan.yaofei@outlook.com>
2024-06-15 21:19:18 +0800

    1.21 初步提交

---
+++ fakeplayer-v1_21_R1/src/main/java/io/github/hello09x/fakeplayer/v1_21_R1/spi/NMSNetworkImpl.java
@@ -0,0 +1,64 @@
+package io.github.hello09x.fakeplayer.v1_21_R1.spi;
+
+import io.github.hello09x.fakeplayer.api.spi.NMSNetwork;
+import io.github.hello09x.fakeplayer.api.spi.NMSServerGamePacketListener;
+import io.github.hello09x.fakeplayer.v1_21_R1.network.DummyConnection;
+import io.github.hello09x.fakeplayer.v1_21_R1.network.DummyServerGamePacketListenerImpl;
+import net.minecraft.network.ConnectionProtocol;
+import net.minecraft.server.network.CommonListenerCookie;
+import org.bukkit.Server;
+import org.bukkit.craftbukkit.v1_21_R1.CraftServer;
+import org.bukkit.craftbukkit.v1_21_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.net.InetAddress;
+
+public class NMSNetworkImpl implements NMSNetwork {
+
+    @NotNull
+    private final DummyConnection connection;
+
+    private NMSServerGamePacketListener serverGamePacketListener;
+
+    public NMSNetworkImpl(
+            @NotNull InetAddress address
+    ) {
+        this.connection = new DummyConnection(address);
+    }
+
+    @NotNull
+    @Override
+    public NMSServerGamePacketListener placeNewPlayer(
+            @NotNull Server server,
+            @NotNull Player player
+    ) {
+        this.connection.setProtocolAttr(ConnectionProtocol.PLAY);
+        var handle = ((CraftPlayer) player).getHandle();
+        var cookie = CommonListenerCookie.createInitial(((CraftPlayer) player).getProfile());
+        var listener = new DummyServerGamePacketListenerImpl(
+                ((CraftServer) server).getServer(),
+                this.connection,
+                handle,
+                cookie
+        );
+        handle.connection = listener;
+        ((CraftServer) server).getHandle().placeNewPlayer(
+                this.connection,
+                handle,
+                cookie
+        );
+        this.serverGamePacketListener = listener;
+        return listener;
+    }
+
+    @NotNull
+    @Override
+    public NMSServerGamePacketListener getServerGamePacketListener() throws IllegalStateException {
+        if (this.serverGamePacketListener == null) {
+            throw new IllegalStateException("not initialized");
+        }
+        return this.serverGamePacketListener;
+    }
+
+}
