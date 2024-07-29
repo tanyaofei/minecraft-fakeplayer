dd108ca6cb84a3f5bfce9dc649c9c1084e4faf70
tanyaofei <tan.yaofei@outlook.com>
2024-07-29 18:10:01 +0800

    redesign update checking

--- fakeplayer-core/src/main/java/io/github/hello09x/fakeplayer/core/util/update/UpdateChecker.java
+++ fakeplayer-core/src/main/java/io/github/hello09x/fakeplayer/core/util/update/UpdateChecker.java
@@ -1,6 +1,8 @@
 package io.github.hello09x.fakeplayer.core.util.update;
 
 import com.google.gson.Gson;
+import io.github.hello09x.devtools.core.version.InvalidVersionException;
+import io.github.hello09x.devtools.core.version.Version;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
@@ -23,33 +25,16 @@ public class UpdateChecker {
         this.repository = repository;
     }
 
-    public static boolean isNew(@NotNull String current, @NotNull String other) {
-        var split1 = current.split("\\.");
-        var split2 = other.split("\\.");
-
-        if (split2.length > split1.length) {
-            // 如果 other 的版本号位数更多, 则认为是新版本号
-            return true;
-        }
-
-        if (split2.length < split1.length) {
-            // 如果 other 的版本号位数更少, 则认为是旧版本
+    public static boolean isNew(@NotNull String local, @NotNull String remote) {
+        Version a, b;
+        try {
+            a = Version.parse(local);
+            b = Version.parse(remote);
+        } catch (InvalidVersionException e) {
             return false;
         }
 
-        // split2.length == split1.length
-        var length = split1.length;
-        for (int i = 0; i < length; i++) {
-            var v1 = Integer.parseInt(split1[i]);
-            var v2 = Integer.parseInt(split2[i]);
-            if (v1 == v2) {
-                continue;
-            }
-
-            return v1 < v2;
-        }
-
-        return false;
+        return a.compareTo(b) < 0;
     }
 
     public @NotNull Release getLastRelease() throws IOException, InterruptedException {
