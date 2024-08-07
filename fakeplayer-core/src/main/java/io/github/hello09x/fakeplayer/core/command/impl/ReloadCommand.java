80ecb887937bbd4a5b4475e6070c1a427127c5a4
tanyaofei <tan.yaofei@outlook.com>
2024-08-07 18:40:18 +0800

    Send warning message if config.yml is not existed when execute /fp reload

--- fakeplayer-core/src/main/java/io/github/hello09x/fakeplayer/core/command/impl/ReloadCommand.java
+++ fakeplayer-core/src/main/java/io/github/hello09x/fakeplayer/core/command/impl/ReloadCommand.java
@@ -8,8 +8,9 @@ import io.github.hello09x.fakeplayer.core.config.FakeplayerConfig;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
+import static net.kyori.adventure.text.Component.text;
 import static net.kyori.adventure.text.Component.translatable;
-import static net.kyori.adventure.text.format.NamedTextColor.GRAY;
+import static net.kyori.adventure.text.format.NamedTextColor.*;
 
 @Singleton
 public class ReloadCommand extends AbstractCommand {
@@ -26,6 +27,13 @@ public class ReloadCommand extends AbstractCommand {
 
     public void reload(@NotNull CommandSender sender, @NotNull CommandArguments args) {
         config.reload();
+        if (!config.isFileExists()) {
+            sender.sendMessage(translatable(
+                    "fakeplayer.command.reload.config-file-not-found",
+                    text(FakeplayerConfig.CONFIG_FILE_NAME, WHITE),
+                    text(FakeplayerConfig.CONFIG_TMPL_FILE_NAME, WHITE)
+            ).color(GOLD));
+        }
         sender.sendMessage(translatable("fakeplayer.command.generic.success", GRAY));
         if (config.isFileConfigurationOutOfDate()) {
             sender.sendMessage(translatable("fakeplayer.configuration.out-of-date", GRAY));
