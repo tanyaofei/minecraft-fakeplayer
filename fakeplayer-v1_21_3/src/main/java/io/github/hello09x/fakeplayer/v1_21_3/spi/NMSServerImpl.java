9a405dd80b01bcaec0590810f560cda17b4e75e9
tanyaofei <tan.yaofei@outlook.com>
2024-11-01 17:50:01 +0800

    Add supports for 1.21.2 and 1.21.3

---
+++ fakeplayer-v1_21_3/src/main/java/io/github/hello09x/fakeplayer/v1_21_3/spi/NMSServerImpl.java
@@ -0,0 +1,38 @@
+package io.github.hello09x.fakeplayer.v1_21_3.spi;
+
+import com.mojang.authlib.GameProfile;
+import io.github.hello09x.devtools.core.utils.WorldUtils;
+import io.github.hello09x.fakeplayer.api.spi.NMSServer;
+import io.github.hello09x.fakeplayer.api.spi.NMSServerPlayer;
+import lombok.Getter;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ClientInformation;
+import net.minecraft.server.level.ServerPlayer;
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.craftbukkit.v1_21_R2.CraftServer;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+public class NMSServerImpl implements NMSServer {
+
+
+    @Getter
+    private final MinecraftServer handle;
+
+    public NMSServerImpl(@NotNull Server server) {
+        this.handle = ((CraftServer) server).getServer();
+    }
+
+    @Override
+    public @NotNull NMSServerPlayer newPlayer(@NotNull UUID uuid, @NotNull String name) {
+        var handle = new ServerPlayer(
+                new NMSServerImpl(Bukkit.getServer()).getHandle(),
+                new NMSServerLevelImpl(WorldUtils.getMainWorld()).getHandle(),
+                new GameProfile(uuid, name),
+                ClientInformation.createDefault()
+        );
+        return new NMSServerPlayerImpl(handle.getBukkitEntity());
+    }
+}
