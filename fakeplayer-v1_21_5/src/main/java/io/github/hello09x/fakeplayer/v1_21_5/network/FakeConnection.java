7997a368041a0fd2c1b0d15dd644510a4b0fbe43
tanyaofei <tan.yaofei@outlook.com>
2025-05-12 11:01:52 +0800

    Add supports for 1.21.5

---
+++ fakeplayer-v1_21_5/src/main/java/io/github/hello09x/fakeplayer/v1_21_5/network/FakeConnection.java
@@ -0,0 +1,43 @@
+package io.github.hello09x.fakeplayer.v1_21_5.network;
+
+import io.github.hello09x.fakeplayer.core.Main;
+import io.github.hello09x.fakeplayer.core.manager.FakeplayerManager;
+import io.github.hello09x.fakeplayer.core.network.FakeChannel;
+import net.minecraft.network.Connection;
+import net.minecraft.network.PacketSendListener;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.PacketFlow;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.net.InetAddress;
+import java.util.logging.Logger;
+
+public class FakeConnection extends Connection {
+
+    private final static Logger log = Main.getInstance().getLogger();
+    private final FakeplayerManager manager = Main.getInjector().getInstance(FakeplayerManager.class);
+
+    public FakeConnection(@NotNull InetAddress address) {
+        super(PacketFlow.SERVERBOUND);
+        this.channel = new FakeChannel(null, address);
+        this.address = this.channel.remoteAddress();
+        Connection.configureSerialization(this.channel.pipeline(), PacketFlow.SERVERBOUND, false, null);
+    }
+
+    @Override
+    public boolean isConnected() {
+        return true;
+    }
+
+    @Override
+    public void send(Packet<?> packet, @Nullable PacketSendListener listener) {
+
+    }
+
+    @Override
+    public void send(Packet<?> packet) {
+
+    }
+
+}
---
+++ fakeplayer-v1_21_5/src/main/java/io/github/hello09x/fakeplayer/v1_21_5/network/FakePlayerAdvancements.java
@@ -0,0 +1,64 @@
+package io.github.hello09x.fakeplayer.v1_21_5.network;
+
+import com.mojang.datafixers.DataFixer;
+import net.minecraft.advancements.AdvancementHolder;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.server.PlayerAdvancements;
+import net.minecraft.server.ServerAdvancementManager;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.players.PlayerList;
+
+import java.nio.file.Path;
+
+public class FakePlayerAdvancements extends PlayerAdvancements {
+
+    public FakePlayerAdvancements(
+            DataFixer datafixer,
+            PlayerList playerlist,
+            ServerAdvancementManager manager,
+            Path path,
+            ServerPlayer player
+    ) {
+        super(datafixer, playerlist, manager, path, player);
+        this.save();
+    }
+
+    @Override
+    public boolean award(AdvancementHolder advancementholder, String s) {
+        return false;
+    }
+
+    @Override
+    public void flushDirty(ServerPlayer entityplayer, boolean flag) {
+
+    }
+
+    @Override
+    public AdvancementProgress getOrStartProgress(AdvancementHolder advancement) {
+        return new AdvancementProgress();
+    }
+
+    @Override
+    public boolean revoke(AdvancementHolder advancement, String s) {
+        return false;
+    }
+
+    @Override
+    public void save() {
+    }
+
+    @Override
+    public void setPlayer(ServerPlayer player) {
+    }
+
+    @Override
+    public void setSelectedTab(AdvancementHolder advancement) {
+    }
+
+    @Override
+    public void stopListening() {
+
+    }
+
+
+}
