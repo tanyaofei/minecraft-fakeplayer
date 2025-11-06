88dd479a55908d0de691193832bef5054a349961
tanyaofei <tan.yaofei@outlook.com>
2025-11-06 17:42:18 +0800

    Add supports for 1.21.9 and 1.21.10

---
+++ fakeplayer-v1_21_9/src/main/java/io/github/hello09x/fakeplayer/v1_21_9/spi/NMSServerLevelImpl.java
@@ -0,0 +1,19 @@
+package io.github.hello09x.fakeplayer.v1_21_9.spi;
+
+import io.github.hello09x.fakeplayer.api.spi.NMSServerLevel;
+import lombok.Getter;
+import net.minecraft.server.level.ServerLevel;
+import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_21_R6.CraftWorld;
+import org.jetbrains.annotations.NotNull;
+
+public class NMSServerLevelImpl implements NMSServerLevel {
+
+    @Getter
+    private final ServerLevel handle;
+
+    public NMSServerLevelImpl(@NotNull World world) {
+        this.handle = ((CraftWorld) world).getHandle();
+    }
+
+}
